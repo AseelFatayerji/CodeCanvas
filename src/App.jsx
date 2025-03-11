43e7b16335c8808cc7e6657adfe7f825ef36bfce
AseelFatayerji <fatayerjiaseel@gmail.com>
2025-03-11 02:17:17 +0200

    fixed filing

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { Routes, Route } from "react-router-dom";
-import "./App.css";
+import "./css/App.css";
 import { Home, About, Contact, Gallery, Projects, Services } from "./pages";
 import Navbar from "./components/navbar";
 
