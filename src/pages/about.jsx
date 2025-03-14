4448f615e3fa2b2a61cd36f0fbb4f03965a460c9
AseelFatayerji <fatayerjiaseel@gmail.com>
2025-03-14 22:36:34 +0200

    fixed text position

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,11 +1,11 @@
 import React, { Suspense } from "react";
 import { Canvas } from "@react-three/fiber";
-import { Html } from "@react-three/drei";
 
 import "../css/backgrounds.css";
 import Loader from "../components/loader";
 import Computer from "../model_loader/pc_screen";
 import Globe from "../model_loader/globe";
+import AboutText from "../components/about_text";
 
 function About() {
   const screenSize = window.innerWidth < 760;
@@ -15,10 +15,10 @@ function About() {
     let rotation = [0, 0, 0];
     if (screenSize) {
       screenScale = [0.2, 0.2, 0.2];
-      screenPose = [0, -15, -25];
+      screenPose = [-0.1, -15, -20];
     } else {
       screenScale = [1.7, 1.7, 1.7];
-      screenPose = [-1, -124, -120];
+      screenPose = [-1, -124, -115];
     }
     return [screenScale, screenPose, rotation];
   };
@@ -40,30 +40,7 @@ function About() {
             scale={modelScale}
             rotation={rotation}
           />
-          <Html>
-            <div
-              className={
-                screenSize
-                  ? "inset-0 fixed text-white text-sm flex self-center justify-self-center rounded-lg backdrop-blur-md bg-white/5 w-80 h-60 p-2"
-                  : "inset-0 fixed text-white text-m flex self-center justify-self-center w-93 h-68 mb-10 p-2"
-              }
-            >
-              <div className="w-full h-full ">
-                <div className="w-full flex justify-center items-center">
-                  <img
-                    src="src\assets\backgrounds\character.png"
-                    className={`h-full ${screenSize ? "w-25 mb-2" : "w-30 mb-3"}`}
-                  />
-                </div>
-                <div className="text-center font-bold text-pretty w-full">
-                  I'm Aseel Fatayerji, an artist specializing in concept art,
-                  and 3D modeling. I create colorful worlds and unique
-                  characters, by blending traditional techniques with digital
-                  tools. Let’s collaborate to bring your vision to life!.
-                </div>
-              </div>
-            </div>
-          </Html>
+          <AboutText screenSize={screenSize} />
         </Suspense>
       </Canvas>
     </section>
