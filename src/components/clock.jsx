47b853c04004cc64e0914f4265ceda2025e57872
AseelFatayerji <fatayerjiaseel@gmail.com>
2025-03-23 14:24:57 +0200

    added clock component

--- src/components/clock.jsx
+++ src/components/clock.jsx
@@ -1,9 +1,15 @@
 import React, { useState, useEffect } from "react";
 
-
 function Clock() {
-    
-    return ( <div>Clock</div> );
+  return (
+    <div className="inset-0 fixed self-end justify-self-center mr-80 mb-15">
+      <div className="">
+        <div>
+          <spn>00:00</spn>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Clock;
