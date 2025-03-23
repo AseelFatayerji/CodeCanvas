df075a45dfa8279a72227e276fde3cf75f20523a
AseelFatayerji <fatayerjiaseel@gmail.com>
2025-03-23 14:19:56 +0200

    adjusted text position

--- src/components/about_text.jsx
+++ src/components/about_text.jsx
@@ -1,30 +1,28 @@
 import { Html } from "@react-three/drei";
+import Clock from "./clock";
 
 function AboutText(props) {
   return (
     <Html>
       <div
         className={`inset-0 fixed text-white text-sm flex self-center justify-self-center ${
-          props.screenSize ? "w-80 h-60 p-2" : "w-95 h-68 mb-40 p-3"
+          props.screenSize ? "w-90 h-60 p-4" : "w-95 h-68 mb-37 p-3"
         }`}
       >
-        <div className="w-full h-full ">
-          <div className="w-full flex justify-center items-center">
-            <img
-              src="src\assets\backgrounds\character.png"
-              className={`mb-2 rounded-full ${
-                props.screenSize ? "h-25 w-20" : "h-30 w-25 "
-              }`}
-            />
-          </div>
-          <div className="text-center font-bold text-pretty w-full">
-            I'm Aseel Fatayerji, an artist specializing in concept art, and 3D
-            modeling. I create colorful worlds and unique characters, by
-            blending traditional techniques with digital tools. Let’s
-            collaborate to bring your vision to life!.
-          </div>
+        <div>
+          <img
+            src="src\assets\backgrounds\character.png"
+            className={`mb-2 rounded-lg h-50 w-45`}
+          />
+        </div>
+        <div className="p-3 font-bold text-pretty w-full">
+          I'm Aseel Fatayerji, an artist specializing in concept art, and 3D
+          modeling. I create colorful worlds and unique characters, by blending
+          traditional techniques with digital tools. Let’s collaborate to bring
+          your vision to life!.
         </div>
       </div>
+      <Clock />
     </Html>
   );
 }
---
+++ src/components/clock.jsx
@@ -0,0 +1,9 @@
+import React, { useState, useEffect } from "react";
+
+
+function Clock() {
+    
+    return ( <div>Clock</div> );
+}
+
+export default Clock;
