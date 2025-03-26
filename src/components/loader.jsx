37cf43a088c26e9910266c776e707de2fd5f65d9
AseelFatayerji <fatayerjiaseel@gmail.com>
2025-03-27 01:09:05 +0200

    customized loader

--- src/components/loader.jsx
+++ src/components/loader.jsx
@@ -1,9 +1,19 @@
 import { Html } from "@react-three/drei";
+import '../css/loader.css';
 
 function Loader() {
   return (
     <Html>
-      <div className="flex justify-center items-center ">testing</div>;
+      <div className="bg-gray-900 w-screen h-screen ">
+        <div class="spinner fixed inset-0 self-center justify-self-center">
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+      </div>
     </Html>
   );
 }
