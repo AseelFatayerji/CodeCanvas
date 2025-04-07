2c1fb29c961bd522452134ef0a0a98fc0229b731
AseelFatayerji <fatayerjiaseel@gmail.com>
2025-04-07 13:42:59 +0300

    changed card background

--- src/components/items/image-card.jsx
+++ src/components/items/image-card.jsx
@@ -4,11 +4,11 @@ import "../../css/carousal.css";
 
 const ImageCard = ({ src }) => {
     return (
-        <motion.div className='min-w-1/3 ml-3'>
-            <div className="card bg-gray-800 rounded-2xl p-2" id="card">
+        <motion.div className='min-w-1/3'>
+            <div className="card bg-gray-800/50 rounded-lg p-2 ml-2" id="card">
                 <div className="content">
-                    <img src={src} className='h-40 w-30 rounded-lg' />
-                    <div className='pt-5 justify-center text-center'>
+                    <img src={src} className='h-30 w-25 rounded-lg' />
+                    <div className='pt-1 justify-center text-center'>
                         Expand Button
                     </div>
                 </div>
