74e922125bb6a8c54bc4f1ccb9c8c7ca1f1c917e
AseelFatayerji <fatayerjiaseel@gmail.com>
2025-04-07 13:30:40 +0300

    changed image card styling

--- src/components/items/image-card.jsx
+++ src/components/items/image-card.jsx
@@ -5,7 +5,7 @@ import "../../css/carousal.css";
 const ImageCard = ({ src }) => {
     return (
         <motion.div className='min-w-1/3 ml-3'>
-            <div className="card rounded-2xl p-2" id="card">
+            <div className="card bg-gray-800 rounded-2xl p-2" id="card">
                 <div className="content">
                     <img src={src} className='h-40 w-30 rounded-lg' />
                     <div className='pt-5 justify-center text-center'>
