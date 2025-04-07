5c090a77690794e9d5afd86f296a166f2d880760
AseelFatayerji <fatayerjiaseel@gmail.com>
2025-04-07 22:52:06 +0300

    added zoom in button

--- src/components/items/image-card.jsx
+++ src/components/items/image-card.jsx
@@ -1,16 +1,16 @@
 import React, { useState } from 'react';
 import { motion } from 'framer-motion';
 import "../../css/carousal.css";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUpRightAndDownLeftFromCenter } from '@fortawesome/free-solid-svg-icons';
 
 const ImageCard = ({ src }) => {
     return (
         <motion.div className='min-w-1/3'>
             <div className="card bg-gray-800/50 rounded-lg p-2 ml-2" id="card">
-                <div className="content">
+                <div className="content text-right">
                     <img src={src} className='h-30 w-25 rounded-lg' />
-                    <div className='pt-1 justify-center text-center'>
-                        Expand Button
-                    </div>
+                    <FontAwesomeIcon icon={faUpRightAndDownLeftFromCenter} className='mt-1 p-1 rounded-lg text-s hover:bg-white hover:text-gray-800' />
                 </div>
             </div>
         </motion.div>
