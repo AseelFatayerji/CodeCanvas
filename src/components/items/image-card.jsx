e9ec85fdfa2b738d600941c13c4324cf2ed3334b
AseelFatayerji <fatayerjiaseel@gmail.com>
2025-04-09 14:04:00 +0300

    added image view card

--- src/components/items/image-card.jsx
+++ src/components/items/image-card.jsx
@@ -1,19 +1,25 @@
 import React, { useState } from 'react';
 import { motion } from 'framer-motion';
+import Image from './image_discription';
 import "../../css/carousal.css";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faUpRightAndDownLeftFromCenter } from '@fortawesome/free-solid-svg-icons';
 
-const ImageCard = ({ src }) => {
+const ImageCard = ({ src, desc}) => {    
+    const [showImage, setShowImage] = useState(false);
     return (
-        <motion.div className='min-w-1/3'>
-            <div className="card bg-gray-800/50 rounded-lg p-2 ml-2" id="card">
-                <div className="content text-right">
-                    <img src={src} className='h-30 w-25 rounded-lg' />
-                    <FontAwesomeIcon icon={faUpRightAndDownLeftFromCenter} className='mt-1 p-1 rounded-lg text-s hover:bg-white hover:text-gray-800' />
+        <>
+            <motion.div className='min-w-1/3'>
+                <div className="card bg-gray-800/50 rounded-lg p-2 ml-2" id="card">
+                    <div className="content text-right">
+                        <img src={src} className='h-30 w-25 rounded-lg -z-1' />
+                        <FontAwesomeIcon icon={faUpRightAndDownLeftFromCenter} className='mt-1 p-1 rounded-lg text-s hover:bg-white hover:text-gray-800 cursor-pointer' onClick={() => { setShowImage(true) }} />
+                    </div>
                 </div>
-            </div>
-        </motion.div>
+            </motion.div>
+
+            {showImage ? <motion.div ><Image src={src} desc={desc} showImage={showImage} setShowImage={setShowImage}/></motion.div> : <></>}
+        </>
     );
 };
 
---
+++ src/components/items/image_discription.jsx
@@ -0,0 +1,18 @@
+import { faX, faXmark, faXmarkCircle } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import React, { useState } from 'react';
+
+function Image({ src, desc, showImage, setShowImage }) {
+
+    return (
+        <>{showImage ? <div className='fixed inset-0 z-10  bg-white/30 w-full h-full'>
+            <div className='bg-amber-700 w-fit h-fit p-3 self-center justify-self-center rounded-2xl mt-20 text-right '>
+                <FontAwesomeIcon icon={faXmarkCircle} className='rounded-lg text-2xl text-gray-400 hover:text-gray-800 cursor-pointer' onClick={() => { setShowImage(false) }} />
+                <img src={src} className='h-100 w-80 rounded-2xl' />
+                <h1></h1>
+            </div>
+        </div> : <></>}</>
+    );
+}
+
+export default Image;
