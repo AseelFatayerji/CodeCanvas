4cd196a0e9a4fe4812a709274d25390c03ee7eba
AseelFatayerji <fatayerjiaseel@gmail.com>
2025-04-11 00:51:38 +0300

    added zoom in function

--- src/components/items/image_discription.jsx
+++ src/components/items/image_discription.jsx
@@ -1,21 +1,60 @@
-import { faX, faXmark, faXmarkCircle } from '@fortawesome/free-solid-svg-icons';
+import { faXmark } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React, { useState } from 'react';
 
 function Image({ src, desc, setShowImage }) {
-    return (
-        <div className={`fixed inset-0 z-10  bg-white/30`} onClick={() => { setShowImage(false) }}>
+    const [isZoomed, setIsZoomed] = useState(false);
+    const [transformOrigin, setTransformOrigin] = useState('center center');
 
-            <div class="relative border-3 mt-24 border-black shadow-[12px_12px_0px_#000000] overflow-hidden transition-all ease-in-out duration-300 transform -translate-x-6 -translate-y-6 bg-blue-900 w-fit h-fit p-2 self-center justify-self-center rounded-2xl text-right">
-                <div >
-                    <FontAwesomeIcon icon={faXmark} className='text-lg text-white border-3 bg-gray-400 cursor-pointer p-2 mb-2 rounded-md border-black shadow-[3px_3px_0px_#000000] font-extrabold transition-all ease-in-out duration-300 hover:translate-x-[1.5px] hover:translate-y-[1.5px] hover:shadow-[1.5px_1.5px_0px_#000000] hover:bg-gray-800 active:translate-x-[3px] active:shadow-nonemt-2.5 active:translate-y-[3px] active:shadow-none' onClick={() => { setShowImage(false) }} />
+    const handleImageClick = (e) => {
+        const rect = e.target.getBoundingClientRect();
+        const xPercent = ((e.clientX - rect.left) / rect.width) * 100;
+        const yPercent = ((e.clientY - rect.top) / rect.height) * 100;
+        setTransformOrigin(`${xPercent}% ${yPercent}%`);
+        setIsZoomed(!isZoomed);
+    };
+
+    return (
+        <div
+            className="fixed inset-0 z-10 bg-white/30 flex items-center justify-center"
+            onClick={() => {
+                if (isZoomed) {
+                    setIsZoomed(false);
+                } else {
+                    setShowImage(false);
+                }
+            }}
+        >
+            <div
+                className="relative border-3 mt-20 border-black shadow-[12px_12px_0px_#000000] overflow-hidden transition-all ease-in-out duration-300 transform -translate-x-6 -translate-y-6 bg-blue-900 w-fit h-fit p-2 rounded-2xl text-right"
+                onClick={(e) => e.stopPropagation()}
+            >
+                <div>
+                    <FontAwesomeIcon
+                        icon={faXmark}
+                        className="text-lg text-white border-3 bg-gray-400 cursor-pointer p-2 mb-2 rounded-md border-black shadow-[3px_3px_0px_#000000] font-extrabold transition-all ease-in-out duration-300 hover:translate-x-[1.5px] hover:translate-y-[1.5px] hover:shadow-[1.5px_1.5px_0px_#000000] hover:bg-gray-800 active:translate-x-[3px] active:translate-y-[3px] active:shadow-none"
+                        onClick={() => setShowImage(false)}
+                    />
                 </div>
-                <div class="content ">
-                    <img src={src} className='h-95 w-85 rounded-2xl' />
-                    <div className='text-lg text-wrap w-80 text-center p-1 mt-1'>Medium Used: {desc}</div>
+                <div className="content w-full h-full">
+                    <img
+                        src={src}
+                        alt="Zoomable"
+                        onClick={handleImageClick}
+                        className="h-110 w-90 rounded-2xl cursor-zoom-in transition-transform duration-300 ease-in-out"
+                        style={{
+                            transform: isZoomed ? 'scale(3)' : 'scale(1)',
+                            transformOrigin: transformOrigin,
+                            cursor: isZoomed ? 'zoom-out' : 'zoom-in',
+                        }}
+                    />
+                    <div className="text-lg text-white text-center p-1 mt-1">
+                        Medium Used: {desc}
+                    </div>
                 </div>
             </div>
         </div>
     );
 }
+
 export default Image;
