51919eb9d78285f96c7f293d6cce760b66a2e9fe
AseelFatayerji <fatayerjiaseel@gmail.com>
2025-04-14 14:31:06 +0300

    added tailwind and cleanedup css file

--- src/components/items/projects_timeline.jsx
+++ src/components/items/projects_timeline.jsx
@@ -9,14 +9,14 @@ function Timeline() {
     return (
         <Html>
             <div className="inset-0 fixed justify-center items-center flex flex-col font-bold z-10">
-                <div className="overflow-hidden h-fit w-fit scale-54 mb-35">
-                    <motion.ul drag="y"
+                <div className="overflow-hidden h-fit w-fit scale-54 mb-35 text-white">
+                    <motion.ul className="time-container p-0 m-0 grid content-center list-none" drag="y"
                         dragConstraints={{
                             top:(-total*(120)),
                             bottom: 0,
                         }}>
                         {prjs.map((project, index) => {
-                            return <li key={index}>
+                            return <li key={index} className='time-line relative h-fit rounded-full grid col-span-2 text-center font-bold text-4xl p-[20px] border-[20px] border-solid border-transparent'>
                                     <discription>{project.Name}</discription>{project.Disc}</li>
                         })}
 
