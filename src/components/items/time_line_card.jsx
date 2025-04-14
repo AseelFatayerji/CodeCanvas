1807e231a22d52a84fa964cbea46c79a1fed8956
AseelFatayerji <fatayerjiaseel@gmail.com>
2025-04-15 01:21:20 +0300

    changed colors

--- src/components/items/time_line_card.jsx
+++ src/components/items/time_line_card.jsx
@@ -2,8 +2,8 @@ function TimeCard({ disc, title }) {
     return (
         <>
             <div className="w-full h-full text-center items-left overflow-hidden p-3">
-                <div className="max-h-fit bg-purple-600 rounded-t-3xl p-2 text-white flex flex-row justify-start gap-2">
-                    <div className="bg-gray-600 w-[60px] h-[60px] rounded-[10px] top-[10px] left-[10px]"></div>
+                <div className="max-h-fit bg-lime-400 rounded-t-3xl p-2 text-white flex flex-row justify-start gap-2">
+                    <div className="bg-gray-600 w-[60px] h-[70px] rounded-[10px] top-[10px] left-[10px]"></div>
                     <div className="gap-1 flex flex-col">
                         <p className="bg-slate-600 text-[20px] p-1 font-bold max-w-fit h-fit rounded-[5px] border border-gray-800 left-[70px]">
                             {title}
