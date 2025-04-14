03cad7d350ef093daab89ad6add4803a7090b8d1
AseelFatayerji <fatayerjiaseel@gmail.com>
2025-04-15 01:19:34 +0300

    fixed positioning

--- src/components/items/projects_timeline.jsx
+++ src/components/items/projects_timeline.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import "../../css/timeline.css"
 import { Html } from '@react-three/drei';
 import { motion, useMotionValue } from 'framer-motion';
+import TimeCard from './time_line_card';
 
 function Timeline() {
     const [currentIndex, setCurrentIndex] = useState(0);
@@ -47,9 +48,9 @@ function Timeline() {
     }, [currentIndex, y]);
     return (
         <Html>
-            <div className="inset-0 fixed justify-center items-center flex flex-col font-bold z-10">
-                <div className="overflow-hidden h-fit w-fit scale-55 mb-40 text-white">
-                    <motion.ul className={`time-container p-0 m-0 grid content-center list-none ${isMouseDown ? 'cursor-grabbing' : 'cursor-grab'}`} drag="y"
+            <div className="inset-x-0 h-130 -translate-y-25 fixed justify-center items-center flex flex-col font-bold z-10">
+                <div className="overflow-hidden scale-35 text-white">
+                    <motion.ul className={`time-container w-fit p-0 m-0 grid content-center list-none ${isMouseDown ? 'cursor-grabbing' : 'cursor-grab'}`} drag="y"
                         onMouseDown={handleMouseDown}
                         onMouseUp={handleMouseUp}
                         style={{ y }}
@@ -59,7 +60,7 @@ function Timeline() {
                         }}>
                         {prjs.map((project, index) => {
                             return <li key={index} className='time-line text-pretty relative h-fit p-4 rounded-[70px] grid col-span-2 text-center font-bold text-2xl border-[20px] border-solid border-transparent'>
-                                <discription>{project.Name}</discription>{project.Disc}</li>
+                                <TimeCard disc={project.Disc} title={project.Name}/></li>
                         })}
 
                     </motion.ul>
