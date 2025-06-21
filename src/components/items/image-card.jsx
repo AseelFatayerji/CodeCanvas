dd813812421604646f047fa53853ba526b23b811
AseelFatayerji <161306291+AseelFatayerji@users.noreply.github.com>
2025-06-21 19:24:05 +0300

    changed image card design

--- src/components/items/image-card.jsx
+++ src/components/items/image-card.jsx
@@ -1,23 +1,32 @@
-import React, { useState } from 'react';
-import { motion } from 'framer-motion';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faUpRightAndDownLeftFromCenter } from '@fortawesome/free-solid-svg-icons';
+import React, { useState } from "react";
+import { motion } from "framer-motion";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUpRightAndDownLeftFromCenter } from "@fortawesome/free-solid-svg-icons";
 
-const ImageCard = ({ src, onClick, animate="" }) => {
-    return (
-        <>
-            <motion.div className={`min-w-1/3 ${animate}`}>
-                <div className="card bg-gray-800/50 rounded-lg p-2 ml-3 shadow-[7px_7px_0px_0px_rgba(0,_0,_0,_0.2)]" id="card">
-                    <div className="content text-right">
-                        <img src={src} className='h-30 w-25 rounded-lg cursor-pointer' onClick={onClick}/>
-
-                        <FontAwesomeIcon icon={faUpRightAndDownLeftFromCenter} className='cursor-pointer text-center bg-transparent text-gray-300 text-lg border border-gray-300 shadow-[2px_2px_0px_#ffffff] font-medium overflow-hidden relative py-1 px-2 mt-2 rounded-md transition-all ease-in-out duration-300 hover:translate-x-[1.5px] hover:translate-y-[1.5px] hover:shadow-[1.5px_1.5px_0px_#ffffff] hover:brightness-150 active:translate-x-[3px] active:shadow-nonemt-2.5 active:translate-y-[3px] active:shadow-none active:opacity-75 outline-none group' 
-                        onClick={onClick} />
-                    </div>
-                </div>
-            </motion.div>
-        </>
-    );
+const ImageCard = ({ src, onClick, animate = "" }) => {
+  return (
+    <>
+      <motion.div className={`min-w-fit ${animate}`}>
+        <div
+          className="rounded-lg ml-3 border-4 border-gray-800/50 shadow-[7px_7px_0px_0px_rgba(0,_0,_0,_0.2)]"
+          id="card"
+        >
+          <div className="content text-right relative">
+            <img
+              src={src}
+              className="max-w-md h-auto w-48 ms:w-20 rounded-lg cursor-pointer"
+              onClick={onClick}
+            />
+              <FontAwesomeIcon
+                icon={faUpRightAndDownLeftFromCenter}
+                className=" absolute cursor-pointer bottom-2 right-2 text-center bg-gray-800/50  text-gray-300 text-lg border border-gray-300 font-medium overflow-hidden py-1 px-2 mt-2 rounded-md transition-all ease-in-out duration-300 hover:translate-x-[1.5px] hover:translate-y-[1.5px] hover:shadow-[1.5px_1.5px_0px_#ffffff] hover:brightness-150 active:translate-x-[3px] active:shadow-nonemt-2.5 active:translate-y-[3px] active:shadow-none active:opacity-75 outline-none group"
+                onClick={onClick}
+              />
+          </div>
+        </div>
+      </motion.div>
+    </>
+  );
 };
 
 export default ImageCard;
--- src/components/items/image_discription.jsx
+++ src/components/items/image_discription.jsx
@@ -1,6 +1,4 @@
-import { faXmark } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import React, { useState } from 'react';
+import { useState } from 'react';
 
 function Image({ src, desc, setShowImage }) {
     const [isZoomed, setIsZoomed] = useState(false);
@@ -16,7 +14,7 @@ function Image({ src, desc, setShowImage }) {
 
     return (
         <div
-            className="fixed inset-0 z-10 bg-white/30 flex h-full w-full flex-col pl-10 items-center-safe justify-center"
+            className="fixed pixel inset-0 z-10 bg-white/30 flex h-full w-full flex-col pl-10 items-center-safe justify-center cursor-pointer"
             onClick={() => {
                 if (isZoomed) {
                     setIsZoomed(false);
@@ -26,30 +24,23 @@ function Image({ src, desc, setShowImage }) {
             }}
         >
             <div
-                className="border-3 mt-25 border-black shadow-[12px_12px_0px_#000000] overflow-hidden transition-all ease-in-out duration-300 transform -translate-x-6 -translate-y-6 bg-blue-900 w-fit h-fit p-2 rounded-2xl text-right"
+                className="border-3 mt-15 border-black shadow-[12px_12px_0px_#000000] overflow-hidden transition-all ease-in-out duration-300 transform -translate-x-6 -translate-y-6 bg-blue-900 w-fit h-fit p-2 rounded-2xl text-right"
                 onClick={(e) => e.stopPropagation()}
             >
-                <div>
-                    <FontAwesomeIcon
-                        icon={faXmark}
-                        className="text-lg text-white border-3 bg-gray-400 cursor-pointer p-2 mb-2 rounded-md border-black shadow-[3px_3px_0px_#000000] font-extrabold transition-all ease-in-out duration-300 hover:translate-x-[1.5px] hover:translate-y-[1.5px] hover:shadow-[1.5px_1.5px_0px_#000000] hover:bg-gray-800 active:translate-x-[3px] active:translate-y-[3px] active:shadow-none"
-                        onClick={() => setShowImage(false)}
-                    />
-                </div>
-                <div className="content w-full h-full">
+                <div className="content w-full h-full flex-col">
                     <img
                         src={src}
                         alt="Zoomable"
                         onClick={handleImageClick}
-                        className="h-110 w-90 rounded-2xl cursor-zoom-in transition-transform duration-300 ease-in-out"
+                        className="max-w-md h-auto w-full rounded-2xl cursor-zoom-in transition-transform duration-300 ease-in-out"
                         style={{
                             transform: isZoomed ? 'scale(3)' : 'scale(1)',
                             transformOrigin: transformOrigin,
                             cursor: isZoomed ? 'zoom-out' : 'zoom-in',
                         }}
                     />
-                    <div className="text-lg text-white text-center p-1 mt-1">
-                        Medium Used: {desc}
+                    <div className="text-wrap text-white text-center p-2 mt-1">
+                        Medium Used:{desc}
                     </div>
                 </div>
             </div>
