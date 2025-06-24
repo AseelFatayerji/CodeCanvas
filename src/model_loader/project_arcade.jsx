5935a757e7d62f912e1634c0dc4282f403ff8fac
AseelFatayerji <161306291+AseelFatayerji@users.noreply.github.com>
2025-06-24 22:54:06 +0300

    fixed timeline responsiveness

--- src/model_loader/project_arcade.jsx
+++ src/model_loader/project_arcade.jsx
@@ -1,19 +1,23 @@
 import { useGLTF } from "@react-three/drei";
 import arcade from "../assets/models/project-display.glb";
 import { useRef } from "react";
+import Timeline from "../components/items/projects_timeline";
 
 function Arcade(props) {
   const ref = useRef();
   const { scene, animation } = useGLTF(arcade);
   return (
-    <mesh
+    <group
       ref={ref}
       position={props.position}
       scale={props.scale}
       rotation={props.rotation}
     >
       <primitive object={scene} />
-    </mesh>
+      <group position={[0, 150, 0]} scale={5}>
+        <Timeline />
+      </group>
+    </group>
   );
 }
 
