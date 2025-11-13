594d3c9d273f7b0da7fc44cdbd2e039071fa9d58
AseelFatayerji <161306291+AseelFatayerji@users.noreply.github.com>
2025-11-13 21:55:22 +0200

    Started new page deisgns

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,6 @@
 import { Routes, Route } from "react-router-dom";
 import "./css/App.css";
-import "./css/animation.css";
-import "./css/backgrounds.css";
-import "./css/shapes.css";
-import { Home, About3D, Contact3D, Gallery3D, Projects3D, Services3D } from "./pages";
+import { Home} from "./pages";
 import Navbar from "./components/navbar";
 
 function App() {
@@ -12,11 +9,6 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="3D/About" element={<About3D />} />
-        <Route path="3D/Services" element={<Services3D />} />
-        <Route path="3D/Gallery" element={<Gallery3D />} />
-        <Route path="3D/Projects" element={<Projects3D />} />
-        <Route path="3D/Contact" element={<Contact3D />} />
       </Routes>
     </main>
   );
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,15 +1,3 @@
-// import Services3D from "./3D/services";
-// import Gallery3D from "./3D/gallery";
-// import Projects3D from "./3D/projects";
-// import Contact3D from "./3D/contact";
-// import About3D from "./3D/about";
-
 import Home from "./Home";
-// import Services from "./Parallax/services";
-// import Gallery from "./Parallax/gallery";
-// import Projects from "./Parallax/projects";
-// import Contact from "./Parallax/contact";
-
-
 
 export {Home  };
