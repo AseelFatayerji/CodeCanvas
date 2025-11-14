62d578882987bbd6eb6765ed601b479e951ccc48
AseelFatayerji <161306291+AseelFatayerji@users.noreply.github.com>
2025-11-14 02:42:51 +0200

    fixed model pose

--- src/model_loader/Astronaut.jsx
+++ src/model_loader/Astronaut.jsx
@@ -1,6 +1,8 @@
 import { useAnimations, useGLTF } from "@react-three/drei";
 import astronaut from "../assets/models/astronaut.glb";
-import { useEffect, useRef } from "react";
+import { use, useEffect, useRef } from "react";
+import { useMotionValue, useSpring } from "framer-motion";
+import { useFrame } from "@react-three/fiber";
 
 function Astronaut(prop) {
   const ref = useRef();
@@ -11,12 +13,23 @@ function Astronaut(prop) {
       actions[animations[0].name].play();
     }
   }, [actions, animations]);
+
+  const yPosition = useMotionValue(5);
+  const smooth = useSpring(yPosition, { damping: 30 });
+  useEffect(() => {
+    smooth.set(prop.position[1]);
+  }, [smooth]);
+  
+  useFrame(() => {
+    ref.current.position.y = smooth.get();
+  });
+
   return (
     <group
       ref={ref}
-      position={prop.position || [2, 2, 0]}
-      rotation={[(-Math.PI / 2)+0.2, 0.6, 1]}
-      scale={prop.scale || 1.3}
+      position={prop.position}
+      rotation={[-Math.PI / 2 + 0.2, 0.9, 1.2]}
+      scale={prop.scale}
     >
       <primitive object={scene} />
     </group>
--- src/pages/Hero.jsx
+++ src/pages/Hero.jsx
@@ -4,16 +4,22 @@ import ParallaxBg from "../components/ParallaxBg";
 import Astronaut from "../model_loader/Astronaut";
 import Loader from "../components/loaders/model-loader";
 import { Suspense } from "react";
+import { useMediaQuery } from "react-responsive";
 
 function Hero() {
+  const isMobile = useMediaQuery({ query: "(max-width: 853px)" });
+
   return (
-    <div className="flex justify-center md:justify-start min-h-screen overflow-hidden">
+    <div className="flex justify-center md:justify-start min-h-screen">
       <HeroText />
       <ParallaxBg />
-      <figure className="absolute inset-0 w-[90vw] h-[100vw]">
+      <figure className="absolute inset-0 w-full h-full m-0">
         <Canvas>
           <Suspense fallback={<Loader />}>
-            <Astronaut />
+            <Astronaut
+              position={isMobile ? [0.2, -2.3, 0] : [2, -1, 0]}
+              scale={isMobile ? 1 : 2.7}
+            />
           </Suspense>
         </Canvas>
       </figure>
