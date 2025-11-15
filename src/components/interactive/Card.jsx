7cbc0e1290e8da9b55cb565197128eee56ee5563
AseelFatayerji <161306291+AseelFatayerji@users.noreply.github.com>
2025-11-15 02:56:37 +0200

    created interactive cards

---
+++ src/components/interactive/Card.jsx
@@ -0,0 +1,30 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { motion } from "framer-motion";
+
+function Card(prop) {
+  return prop.img && !prop.text ? (
+    <motion.div
+      whileHover={{ scale: 1.5 }}
+      drag
+      dragConstraints={prop.ref}
+      dragElastic={1}
+      className={`absolute text-5xl cursor-grab `}
+      style={prop.style}
+    >
+      <FontAwesomeIcon icon={prop.img} />
+    </motion.div>
+  ) : (
+    <motion.div
+      whileHover={{ scale: 1.5 }}
+      drag
+      dragConstraints={prop.ref}
+      dragElastic={1}
+      className="absolute px-1 py-4 text-xl text-center rounded-full ring-gray-700 font-extralight bg-[#282b4b] w-48 h-fit cursor-grab "
+      style={prop.style}
+    >
+      {prop.text}
+    </motion.div>
+  );
+}
+
+export default Card;
