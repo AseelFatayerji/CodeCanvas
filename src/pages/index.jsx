345d1cf5f318164f4d0a8a6b21d3e24e0be00353
AseelFatayerji <161306291+AseelFatayerji@users.noreply.github.com>
2025-12-30 16:52:15 +0200

    Updates

--- src/pages/index.jsx
+++
@@ -1,7 +0,0 @@
-import Hero from "./Hero";
-import About from "./About";
-import Services from "./Services";
-import Projects from "./Projects";
-import Contact from "./Contact";
-
-export {Hero, About, Services, Projects, Contact};
